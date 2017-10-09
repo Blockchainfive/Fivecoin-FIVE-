0067daf5f36a389a621e7e416046ba851a74a4a2
Blockchainfive <32436218+Blockchainfive@users.noreply.github.com>
2017-10-09 22:49:11 +1000

    Update CryptoNoteConfig.h
      //"45.77.97.76:55555",
      //"45.77.118.240:55555",

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"45.77.144.177:55555",
-  //"",
+  //"45.77.97.76:55555",
+  //"45.77.118.240:55555",
 };
 
 struct CheckpointData {
